424099ece526b649e235fa6e67d19d27f1d7c693
suyeonkim <suyeonk56@gmail.com>
2025-01-24 20:13:28 +0900

    프로젝트 재생성

---
+++ src/main/java/com/example/schedules_management/SchedulesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedules_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulesManagementApplication.class, args);
+    }
+
+}
